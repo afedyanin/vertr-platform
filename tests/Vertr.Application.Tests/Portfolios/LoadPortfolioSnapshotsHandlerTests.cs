f350d45098127a63b6c97d1b9de017e8e90a3980
DESKTOP-MAIBTV1\A <anfemail@gmail.com>
2025-02-28 15:19:38 +0300

    add test

--- tests/Vertr.Application.Tests/Portfolios/LoadPortfolioSnapshotsHandlerTests.cs
+++ tests/Vertr.Application.Tests/Portfolios/LoadPortfolioSnapshotsHandlerTests.cs
@@ -1,11 +1,25 @@
+using Vertr.Domain.Repositories;
+using Vertr.Application.Portfolios;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace Vertr.Application.Tests.Portfolios;
 
 [TestFixture(Category = "System", Explicit = true)]
-public class LoadPortfolioSnapshotsHandlerTests
+public class LoadPortfolioSnapshotsHandlerTests : ApplicationTestBase
 {
+
     [Test]
-    public void CanLoadAndSavePortfolios()
+    public async Task CanLoadAndSavePortfolios()
     {
+        var request = new LoadPortfolioSnapshotsRequest();
+        var handler = ServiceProvider.GetRequiredService<LoadPortfolioSnapshotsHandler>();
+
+        await handler.Handle(request, CancellationToken.None);
+
+        var repo = ServiceProvider.GetRequiredService<ITinvestPortfolioRepository>();
+
+        var snapshots = repo.Get();
+
         Assert.Pass();
     }
 }

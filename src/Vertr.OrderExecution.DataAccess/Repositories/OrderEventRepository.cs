87cd3235cd96ad721e321e85a36be643ab67a60a
Anatoly Fedyanin <anfemail@gmail.com>
2025-05-16 23:38:44 +0300

    update

--- src/Vertr.OrderExecution.DataAccess/Repositories/OrderEventRepository.cs
+++ src/Vertr.OrderExecution.DataAccess/Repositories/OrderEventRepository.cs
@@ -1,11 +1,21 @@
+using Microsoft.EntityFrameworkCore;
 using Vertr.OrderExecution.Application.Abstractions;
 using Vertr.OrderExecution.Application.Entities;
 
 namespace Vertr.OrderExecution.DataAccess.Repositories;
-internal class OrderEventRepository : IOrderEventRepository
+internal class OrderEventRepository : RepositoryBase, IOrderEventRepository
 {
-    public Task<bool> Save(OrderEvent[] events)
+    public OrderEventRepository(
+        IDbContextFactory<OrderExecutionDbContext> contextFactory)
+        : base(contextFactory)
     {
-        throw new NotImplementedException();
+    }
+
+    public async Task<bool> Save(OrderEvent[] events)
+    {
+        using var context = await GetDbContext();
+        await context.Portfolios.AddRangeAsync(events);
+        var savedRecords = await context.SaveChangesAsync();
+        return savedRecords > 0;
     }
 }

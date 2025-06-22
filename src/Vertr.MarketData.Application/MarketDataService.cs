08118606c0dd106ed44d2ed206b9208bbfca16c2
DESKTOP-MAIBTV1\A <anfemail@gmail.com>
2025-06-22 13:31:42 +0300

    update

--- src/Vertr.MarketData.Application/MarketDataService.cs
+++ src/Vertr.MarketData.Application/MarketDataService.cs
@@ -1,27 +1,31 @@
 using Microsoft.Extensions.Options;
 using Vertr.MarketData.Contracts;
 using Vertr.MarketData.Contracts.Interfaces;
+using Vertr.TinvestGateway.Contracts.Interfaces;
 
 namespace Vertr.MarketData.Application;
 
 internal class MarketDataService : IMarketDataService
 {
     private readonly MarketDataSettings _settings;
+    private readonly ITinvestGatewayMarketData _tinvestGatewayMarketData;
 
     public MarketDataService(
-        IOptions<MarketDataSettings> options
+        IOptions<MarketDataSettings> options,
+        ITinvestGatewayMarketData tinvestGatewayMarketData
         )
     {
         _settings = options.Value;
+        _tinvestGatewayMarketData = tinvestGatewayMarketData;
     }
 
     public Task<CandleSubscription[]> GetSubscriptions()
         => Task.FromResult(_settings.GetCandleSubscriptions());
 
-    public Task<Instrument?> GetInstrument(string instrumentId)
+    public async Task<Instrument?> GetInstrument(string instrumentId)
     {
-        // TODO: Use Redis Cache or call TInvestGateway
-        throw new NotImplementedException();
+        // TODO: Use Redis to Cache instrument
+        var instrument = await _tinvestGatewayMarketData.GetInstrumentById(instrumentId);
+        return instrument;
     }
-
 }

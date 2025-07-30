633d72acc1d7e85c692123d34e1cdc25c93ac99c
DESKTOP-MAIBTV1\A <anfemail@gmail.com>
2025-07-30 20:33:09 +0300

    update

---
+++ src/Vertr.Platform.Host/Components/Common/DemoLogger.cs
@@ -0,0 +1,13 @@
+namespace Vertr.Platform.Host.Components.Common;
+
+public delegate void OnLogHandler(string text);
+
+public static class DemoLogger
+{
+    public static event OnLogHandler? OnLogHandler;
+
+    public static void WriteLine(string text)
+    {
+        OnLogHandler?.Invoke(text);
+    }
+}
--- src/Vertr.Platform.Host/Program.cs
+++ src/Vertr.Platform.Host/Program.cs
@@ -21,6 +21,7 @@ public class Program
         // Add services to the container.
         builder.Services.AddRazorComponents()
             .AddInteractiveServerComponents();
+
         builder.Services.AddFluentUIComponents();
 
         /*

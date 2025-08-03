5221721625be61d112dd0e0320eb644d412b9fb3
DESKTOP-MAIBTV1\A <anfemail@gmail.com>
2025-08-03 12:23:05 +0300

    update

--- tests/Vertr.MarketData.DataAccess.Tests/Repositories/CandlesRepositoryTests.cs
+++ tests/Vertr.MarketData.DataAccess.Tests/Repositories/CandlesRepositoryTests.cs
@@ -15,4 +15,19 @@ public class CandlesRepositoryTests : RepositoryTestBase
         var savedCount = await CandlesRepo.Upsert(candles);
         Assert.That(savedCount, Is.GreaterThan(0));
     }
+
+    [TestCase("e6123145-9665-43e0-8413-cd61b8aa9b13")]
+    public async Task CanGetCandles(string instrumentId)
+    {
+        var candles = await CandlesRepo.Get(Guid.Parse(instrumentId), limit: 50);
+        Assert.That(candles.Count, Is.GreaterThan(0));
+        Assert.That(candles.Count, Is.LessThanOrEqualTo(50));
+    }
+
+    [TestCase("e6123145-9665-43e0-8413-cd61b8aa9b13")]
+    public async Task CanGetLastCandle(string instrumentId)
+    {
+        var candle = await CandlesRepo.GetLast(Guid.Parse(instrumentId));
+        Assert.That(candle, Is.Not.Null);
+    }
 }

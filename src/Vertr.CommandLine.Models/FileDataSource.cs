458054f58f943709ed14f9b2cdf28f754d4d48a0
anatoly-note <anfemail@gmail.com>
2025-12-18 00:35:58 +0300

    update

--- src/Vertr.CommandLine.Models/FileDataSource.cs
+++
@@ -1,8 +0,0 @@
-namespace Vertr.CommandLine.Models;
-
-public record class FileDataSource
-{
-    public required string Symbol { get; init; }
-
-    public required string FilePath { get; init; }
-}

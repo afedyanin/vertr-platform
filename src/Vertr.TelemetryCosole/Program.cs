a6503ae15cc133a8028110c6a88e227d532c30f7
anatoly-note <anfemail@gmail.com>
2026-01-09 13:58:11 +0300

    add otel

---
+++ src/Vertr.TelemetryCosole/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics.Metrics;
+using OpenTelemetry;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Metrics;
+namespace Vertr.TelemetryCosole;
+
+internal sealed class Program
+{
+    static async Task Main(string[] args)
+    {
+        var myMeter = new Meter("MyCompany.MyConsoleApp", "1.0.0");
+        var myCounter = myMeter.CreateCounter<long>("orders_processed_total", description: "Total number of processed orders");
+
+        using var meterProvider = Sdk.CreateMeterProviderBuilder()
+            .AddMeter("MyCompany.MyConsoleApp") // Подписываемся на наш Meter
+            .AddOtlpExporter((options, metricReadeOptions) =>
+            {
+                options.Endpoint = new Uri("http://localhost:9090/api/v1/otlp/v1/metrics");
+                options.Protocol = OtlpExportProtocol.HttpProtobuf;
+
+                metricReadeOptions.PeriodicExportingMetricReaderOptions.ExportIntervalMilliseconds = 1000;
+            })
+            .Build();
+
+        Console.WriteLine("Отправка метрик запущена. Нажмите любую клавишу для выхода...");
+
+        while (!Console.KeyAvailable)
+        {
+            myCounter.Add(1, new KeyValuePair<string, object?>("env", "production"));
+            Console.WriteLine("Метрика 'orders_processed_total' увеличена.");
+            await Task.Delay(5000); // Интервал генерации данных
+        }
+    }
+}

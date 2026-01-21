be7ca827a846a335be53724cd32a65e99a4312d5
DESKTOP-MAIBTV1\A <anfemail@gmail.com>
2026-01-21 14:05:49 +0300

    gauge

---
+++ src/Vertr.Common.Application/Indicators/BasicStatsGauge.cs
@@ -0,0 +1,61 @@
+﻿namespace Vertr.Common.Application.Indicators;
+
+public class BasicStatsGauge
+{
+    private readonly LinkedList<double> _values = new LinkedList<double>();
+
+    private readonly int _maxCount;
+
+    public string Name { get; }
+
+    public int Count => _values.Count;
+
+    public double Last => _values.Last?.Value ?? 0;
+
+    public double Mean { get; private set; }
+
+    public double StdDev { get; private set; }
+
+    public BasicStatsGauge(string name = "", int maxCount = 100)
+    {
+        Name = name;
+        _maxCount = maxCount;
+    }
+
+    public void Add(double nextValue)
+    {
+        if (_values.Count >= _maxCount)
+        {
+            _values.RemoveFirst();
+        }
+
+        _values.AddLast(nextValue);
+
+        CalculateStats();
+    }
+
+    public void Reset()
+    {
+        _values.Clear();
+    }
+
+    private void CalculateStats()
+    {
+        var count = _values.Count;
+
+        if (count == 0)
+        {
+            return;
+        }
+
+        Mean = _values.Average();
+
+        if (count < 2)
+        {
+            return;
+        }
+
+        var sum = _values.Sum(d => (d - Mean) * (d - Mean));
+        StdDev = Math.Sqrt(sum / (count - 1));
+    }
+}
---
+++ tests/Vertr.Common.Aplication.Tests/Indicators/BasicStatsGaugeTests.cs
@@ -0,0 +1,40 @@
+﻿using Vertr.Common.Application.Indicators;
+
+namespace Vertr.Common.Aplication.Tests.Indicators;
+
+public class BasicStatsGaugeTests
+{
+    [Test]
+    public void CanAddNewValue()
+    {
+        var gauge = new BasicStatsGauge();
+
+        var value = 156;
+        gauge.Add(value);
+
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(gauge.Count, Is.EqualTo(1));
+            Assert.That(gauge.Mean, Is.EqualTo(value));
+            Assert.That(gauge.Last, Is.EqualTo(value));
+            Assert.That(gauge.StdDev, Is.EqualTo(0));
+        }
+    }
+
+    [Test]
+    public void CanAddNewValues()
+    {
+        var gauge = new BasicStatsGauge();
+
+        gauge.Add(2);
+        gauge.Add(4);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(gauge.Count, Is.EqualTo(2));
+            Assert.That(gauge.Last, Is.EqualTo(4));
+            Assert.That(gauge.Mean, Is.EqualTo(3));
+            Assert.That(Math.Round(gauge.StdDev, 4), Is.EqualTo(1.4142));
+        });
+    }
+}
